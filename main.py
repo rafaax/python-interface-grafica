4a9ea41595e847ef2908c15432ce062c0abd0be7
rafaax <rafavfx1@gmail.com>
2023-08-01 11:39:22 -0300

    codigo

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ from kivy.app import App
 from kivy.uix.image import Image, AsyncImage
 
 
-class MainApp(App):
+class Main(App):
+    def build(self):
+        img = AsyncImage(source='https://i1.sndcdn.com/visuals-000936297145-w8wYTr-t1240x260.jpg',
+        size_hint=(1, .5),
+        pos_hint={'center_x':.5, 'center_y':.5})
+
+        return img
+
+
+if __name__ == '__main__':
+    app = Main()
+    app.run()
